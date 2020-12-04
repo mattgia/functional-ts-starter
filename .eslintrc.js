1193d85a56ee45b1e58edae9620f83405380399b
Matthew <mattgia3@gmail.com>
2020-12-04 17:12:45 -0500

    stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,25 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    'extends': [
-        'prettier',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+    extends: [
+        "prettier",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 12,
-        'sourceType': 'module'
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    'plugins': [
-        'prettier',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        "prettier/prettier": ["error"],
-        "sort-keys": ["error", "asc", { "caseSensitive": true, "natural": false }],
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+    plugins: ['prettier', '@typescript-eslint'],
+    rules: {
+        "prettier/prettier": ['error'],
+        "sort-keys": ['error', 'asc', { caseSensitive: true, natural: false }],
+        indent: ['error', 4],
+        "linebreak-style": ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
 }
