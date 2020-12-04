4fbf2bf8ed76a8584135c8755d06635d2532df10
Matthew <mattgia3@gmail.com>
2020-12-04 17:12:03 -0500

    first

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'prettier',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'prettier',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        "prettier/prettier": ["error"],
+        "sort-keys": ["error", "asc", { "caseSensitive": true, "natural": false }],
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
