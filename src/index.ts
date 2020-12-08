835604b39259a3373187726c35776409c3851d37
Matthew <mattgia3@gmail.com>
2020-12-08 00:42:54 -0500

    add some dev tools

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import Express from 'express'
 const app: Express.Application = Express()
 
 export default app.get('/test', (req, res) => {
-    return res.send({})
+    return res.send({a:1})
 }).listen(3000)
